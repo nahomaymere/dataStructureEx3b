177dee3383f50c6e25b78b8675e988133325ebca
Nahom Hailu <aymerenahom@gmail.com>
2015-12-10 16:26:41 +0200

    Initial Commit

---
+++ Exercice3forDatastruct/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Exercice3forDatastruct
+//
+//  Created by Nahom Hailu on 10/12/15.
+//  Copyright (c) 2015 Nahom. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
